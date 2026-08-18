b0d95d6dd53fe09404b85af3f1a429a9a30eed71
Jawor <jaworyv@gmail.com>
2026-08-18 16:25:52 +0300

    Dobavil transfer test i invalid test

--- src/main/api/tests/transfer_test.py
+++ src/main/api/tests/transfer_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.main.api.models.account_deposit_request import AccountDepositRequest
 from src.main.api.models.create_user_request import CreateUserRequest
 from src.main.api.models.transfer_request import TransferRequest
@@ -10,7 +12,8 @@ from src.main.api.specs.response_specs import ResponseSpecs
 
 
 
-class TransferTest:
+class TestTransfer:
+    @pytest.mark.api
     def test_transfer_one_user(self):
         create_user_request = CreateUserRequest(username="Transferuser2", password="Pas!sw0rd", role="ROLE_USER")
         CreateUserRequester(
@@ -31,15 +34,63 @@ class TransferTest:
         second_account_id = second_account_create_response.id
 
         account_deposit_request = AccountDepositRequest(accountId=first_account_id, amount=1000.5)
-        response = AccountDepositRequester(
+        account_deposit_response = AccountDepositRequester(
             request_spec=RequestSpecs.auth_headers(username="Transferuser2", password="Pas!sw0rd"),
             response_spec=ResponseSpecs.request_ok()
         ).post(account_deposit_request)
+        account_balance = account_deposit_response.balance
 
         transfer_request = TransferRequest(fromAccountId=first_account_id, toAccountId=second_account_id, amount=500)
         response = TransferRequester(
-            request_spec=
-            response_spec=
-        )
+            request_spec=RequestSpecs.auth_headers(username="Transferuser2", password="Pas!sw0rd"),
+            response_spec=ResponseSpecs.request_ok()
+        ).post(transfer_request)
+
+        assert transfer_request.fromAccountId == response.fromAccountId
+        assert transfer_request.toAccountId == response.toAccountId
+        assert account_balance - transfer_request.amount == response.fromAccountIdBalance
+
+    @pytest.mark.api
+    @pytest.mark.parametrize("amount", [
+        499,
+        10001,
+    ])
+    def test_transfer_one_user_invalid(self, amount):
+        create_user_request = CreateUserRequest(username=f"Transf{amount}", password="Pas!sw0rd", role="ROLE_USER")
+        CreateUserRequester(
+            request_spec=RequestSpecs.auth_headers(username="admin", password="123456"),
+            response_spec=ResponseSpecs.request_ok()
+        ).post(create_user_request)
+
+        first_account_create_response = CreateAccountRequester(
+            request_spec=RequestSpecs.auth_headers(username=f"Transf{amount}", password="Pas!sw0rd"),
+            response_spec=ResponseSpecs.request_created()
+        ).post()
+        first_account_id = first_account_create_response.id
+
+        second_account_create_response = CreateAccountRequester(
+            request_spec=RequestSpecs.auth_headers(username=f"Transf{amount}", password="Pas!sw0rd"),
+            response_spec=ResponseSpecs.request_created()
+        ).post()
+        second_account_id = second_account_create_response.id
+
+        first_account_deposit_request = AccountDepositRequest(accountId=first_account_id, amount=9000)
+        AccountDepositRequester(
+            request_spec=RequestSpecs.auth_headers(username=f"Transf{amount}", password="Pas!sw0rd"),
+            response_spec=ResponseSpecs.request_ok()
+        ).post(first_account_deposit_request)
+
+        second_account_deposit_request = AccountDepositRequest(accountId=first_account_id, amount=9000)
+        account_deposit_response = AccountDepositRequester(
+            request_spec=RequestSpecs.auth_headers(username=f"Transf{amount}", password="Pas!sw0rd"),
+            response_spec=ResponseSpecs.request_ok()
+        ).post(second_account_deposit_request)
+        account_balance = account_deposit_response.balance
+
+        transfer_request = TransferRequest(fromAccountId=first_account_id, toAccountId=second_account_id, amount=amount)
+        response = TransferRequester(
+            request_spec=RequestSpecs.auth_headers(username=f"Transf{amount}", password="Pas!sw0rd"),
+            response_spec=ResponseSpecs.request_bad()
+        ).post(transfer_request)
 
 
